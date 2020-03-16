427435820c012388048ca17f8bf29b39740b6ab9
Rashmi Sharma <rksharma2679@gmail.com>
2020-03-15 21:29:20 -0400

    adding server file

---
+++ server.js
@@ -0,0 +1,17 @@
+// Dependencies
+const express = require("express");
+
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+//sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+      console.log("Server listening on: http://localhost:" + PORT);
+});
