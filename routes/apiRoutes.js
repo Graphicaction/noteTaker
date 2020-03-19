82d6637bc5f74a11077e4ee65cb050fab75e68e0
Rashmi Sharma <rksharma2679@gmail.com>
2020-03-19 16:11:21 -0400

    Updating with async await

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,12 +6,12 @@ const createDb = path.resolve(__dirname, "../db/db.json");
 
 module.exports = function(app) {
     //Getting all notes from notes json file
-    app.get("/api/notes", (req, res) => {
-        let dbNotes = fs.readFileSync(path.resolve(createDb), "utf8");
+    app.get("/api/notes", async (req, res) => {
+        let dbNotes = await fs.readFileSync(path.resolve(createDb), "utf8");
         res.json(JSON.parse(dbNotes));
     });
     //Posting new note to notes json file
-    app.post("/api/notes", (req, res) => {
+    app.post("/api/notes", async (req, res) => {
         const notes = [];
         //updating notes[] with db.json values
         db.forEach(element => {
@@ -25,14 +25,14 @@ module.exports = function(app) {
         newNote.id = id + 1;
         notes.push(newNote);
         //write to file db.json
-        fs.writeFileSync(createDb,JSON.stringify(notes), (err) => {
+        await fs.writeFileSync(createDb,JSON.stringify(notes), (err) => {
             // throws an error, you could also catch it here
             if (err) throw err;
         });
         res.json(newNote);            
     });
     //delting a note from notes json file
-    app.delete("/api/notes/:id", (req, res) => {
+    app.delete("/api/notes/:id", async (req, res) => {
         const notes = [];
         //updating notes[] with db.json values
         db.forEach(element => {
@@ -43,13 +43,13 @@ module.exports = function(app) {
             if(noteId == note.id){
                 notes.splice(i,1);
                 console.log(`Record "${note.title}" is deleted`); 
-                //write to file db.json
-                fs.writeFileSync(createDb,JSON.stringify(notes), (err) => {
-                    // throws an error, you could also catch it here
-                    if (err) throw err;
-                });
-                res.json(notes);
             }
         });
+        //write to file db.json
+        await fs.writeFileSync(createDb,JSON.stringify(notes), (err) => {
+            // throws an error, you could also catch it here
+            if (err) throw err;
+        });
+        res.json(notes);
     });
 }
